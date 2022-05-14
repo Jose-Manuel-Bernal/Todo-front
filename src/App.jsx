0d7f1f470edfeace63e04a2b6c4cc4f15267d3cb
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-05-14 04:39:52 -0500

    Addaptations made in order to run the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import CategoryList from './components/category/CategoryList'
-import StoreProvider from './state/StoreProvider'
+import { useState } from "react";
+import CategoryList from "./components/category/CategoryList";
+import StoreProvider from "./state/StoreProvider";
 
 function App() {
-
   return (
     <StoreProvider>
       <CategoryList />
     </StoreProvider>
-  )
+  );
 }
 
-export default App
+export default App;
